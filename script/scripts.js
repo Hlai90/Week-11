e032433779bd098fc8e3bddfc6947bd3edb06230
Howard <Howard@gmail.com>
2023-03-20 16:14:04 -0700

    Created a function with new element created

--- script/scripts.js
+++ script/scripts.js
@@ -20,26 +20,40 @@
 
 // greet('Howard', 'night')
 
-const radiusPara = document.querySelector("#radius");
-console.log(radiusPara)
-const resultPara = document.querySelector("#result");
-console.log(resultPara)
+// const radiusPara = document.querySelector("#radius");
+// console.log(radiusPara)
+// const resultPara = document.querySelector("#result");
+// console.log(resultPara)
 
-function circleArea(radius){
-    if(isNaN(radius)){
-        // alert("This is not a string")
-        // update the textConent of innerText property of resultPara
-        resultPara.textContent = "This is not a number"
-    } else {
-    area = Math.PI*radius*radius;
-    return area;
-    }
+// function circleArea(radius){
+//     if(isNaN(radius)){
+//         // alert("This is not a string")
+//         // update the textConent of innerText property of resultPara
+//         resultPara.textContent = "This is not a number"
+//     } else {
+//     area = Math.PI*radius*radius;
+//     return area;
+//     }
+// }
+// let input = prompt("Enter a raidus ")
+// radiusPara.textContent = radiusPara.textContent += input
+
+// let result = circleArea(input)
+// //Show an alert message with both values, radius and area.
+// if (result !== undefined){}
+// // alert(`The area of a circle with a radius of ${radius} is ${result}`)
+// resultPara.textContent = `The radius is ${input} and the area is ${area}`
+const ullist = document.querySelector(".shopping")
+
+function populateList(shoppingListArray){
+    //1 for each item in the shoppingListArray, add a list item with its value
+    for (let item of shoppingListArray);
+    //2 Create a new Li
+    const newli = document.createElement("li")
+        //3 update the Li's text using item
+        newli.textContent = item;
+        ullist.appendChild(newli);
 }
-let input = prompt("Enter a raidus ")
-radiusPara.textContent = radiusPara.textContent += input
 
-let result = circleArea(input)
-//Show an alert message with both values, radius and area.
-if (result !== undefined){}
-// alert(`The area of a circle with a radius of ${radius} is ${result}`)
-resultPara.textContent = `The radius is ${input} and the area is ${area}`
+let myShoppingList = ["Cheese","Bread","Green Pepper"]
+populateList(myShoppingList)
