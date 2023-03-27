b8caa0062ce29a2e953ba721457e0d163c62b53c
Howard <Howard@gmail.com>
2023-03-27 14:56:38 -0700

    created green text

--- script/scripts.js
+++ script/scripts.js
@@ -43,33 +43,53 @@
 // // if (result !== undefined){}
 // // // alert(`The area of a circle with a radius of ${radius} is ${result}`)
 // // resultPara.textContent = `The radius is ${input} and the area is ${area}`
-// const ullist = document.querySelector(".shopping")
+const ullist = document.querySelector(".shopping");
 
-// function populateList(shoppingListArray){
-//     //1 for each item in the shoppingListArray, add a list item with its value
-//     for (let item of shoppingListArray) {
-//     //2 Create a new Li
-//     const newli = document.createElement("li");
-//         //3 update the Li's text using item
-//         newli.textContent = item;
-//         ullist.appendChild(newli);
-//     }
-// }
+function populateList(shoppingListArray) {
+	//1 for each item in the shoppingListArray, add a list item with its value
+	for (let item of shoppingListArray) {
+		//2 Create a new Li
+		const newli = document.createElement("li");
+		//3 update the Li's text using item
+		newli.textContent = item;
+		ullist.appendChild(newli);
+	}
+}
 
-// let myShoppingList = ["Cheese","Bread","Green Pepper"]
-// populateList(myShoppingList);
+let myShoppingList = ["Cheese", "Bread", "Green Pepper"];
+populateList(myShoppingList);
 
-// function squareListMaker() {
-//     ullist.classList.add("squareList");
-//     ullist.classList.remove("circleList");
-// }
-// squareListMaker()
+function squareListMaker() {
+	ullist.classList.add("squareList");
+	ullist.classList.remove("circleList");
+}
+squareListMaker();
 
 function updateImage() {
-    const image = document.querySelector("#shoppingCart");
-    image.setAttribute("src",
-    "https://cdn-icons-png.flaticon.com/512/263/263142.png");
-    image.setAttribute("height", 50)
-    image.setAttribute("width", 50)
-    image.setAttribute("alt", "shopping cart list")
+	const image = document.querySelector("#shoppingCart");
+	image.setAttribute(
+		"src",
+		"https://cdn-icons-png.flaticon.com/512/263/263142.png"
+	);
+	image.setAttribute("height", 50);
+	image.setAttribute("width", 50);
+	image.setAttribute("alt", "shopping cart list");
 }
+updateImage();
+
+function makeListGreen() {
+	//1. Find all the li s -> array-like structure
+	//2. loop through the array
+	//3. Find the textContent of each item of the array
+	//4. update the style of that element to green text
+
+	const someList = document.querySelectorAll(" .shopping > li");
+	console.log(someList);
+	for (let groceries of someList) {
+		let text = groceries.textContent;
+		if (text.includes("Green")) {
+			groceries.classList.add("greenText");
+		}
+	}
+}
+makeListGreen();
